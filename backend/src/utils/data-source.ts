96000e534ae606adbcd29e3978dc05603d5d4f62
Leonhard Francesco Sensi <frank@Leonhards-MacBook-Pro.local>
2023-07-28 15:25:32 +0200

    setup authentication

--- backend/src/utils/data-source.ts
+++ backend/src/utils/data-source.ts
@@ -1,24 +1,7 @@
-import "reflect-metadata";
-import { DataSource } from "typeorm";
-import { User } from "../entities/User";
-
-// export const AppDataSource = new DataSource({
-//   type: "postgres",
-//   host: "localhost",
-//   port: 5432,
-//   username: "test",
-//   password: "test",
-//   database: "test",
-//   synchronize: true,
-//   logging: false,
-//   entities: [User],
-//   migrations: [],
-//   subscribers: [],
-// });
-
-require("dotenv").config();
-import "reflect-metadata";
-import config from "config";
+require('dotenv').config();
+import 'reflect-metadata';
+import { DataSource } from 'typeorm';
+import config from 'config';
 
 const postgresConfig = config.get<{
   host: string;
@@ -26,14 +9,14 @@ const postgresConfig = config.get<{
   username: string;
   password: string;
   database: string;
-}>("postgresConfig");
+}>('postgresConfig');
 
 export const AppDataSource = new DataSource({
   ...postgresConfig,
-  type: "postgres",
+  type: 'postgres',
   synchronize: false,
   logging: false,
-  entities: ["src/entities/**/*.entity{.ts,.js}"],
-  migrations: ["src/migrations/**/*{.ts,.js}"],
-  subscribers: ["src/subscribers/**/*{.ts,.js}"],
+  entities: ['src/entities/**/*.entity{.ts,.js}'],
+  migrations: ['src/migrations/**/*{.ts,.js}'],
+  subscribers: ['src/subscribers/**/*{.ts,.js}'],
 });
